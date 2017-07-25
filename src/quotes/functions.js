7b15e4a8d7f611352d1e00290b5168920f0684e9
harshm9497 <lostinmyworld9497@gmail.com>
2017-07-25 22:51:24 +0530

    adding pagination functionality

--- src/quotes/functions.js
+++ src/quotes/functions.js
@@ -1,27 +1,59 @@
+
 function appendQuote(data)
 {
      for(var quoteCategory in data)
         {
           
-          var item = '<div class="box col-md-3 " data-cat="'+data[quoteCategory].category+'"><h3>'+data[quoteCategory].category+'</h3></div>';
+          var item = '<div class="box col-md-3 categories " data-cat="'+data[quoteCategory].category+'"><h3>'+data[quoteCategory].category+'</h3></div>';
           $(".injectQuote").append(item);
         }
 }
-function showQuotes(object)
-{
-    var typeQuotes  = $(object).data("cat")+".json";
-    console.log(typeQuotes);
+  function showQuotes()
+  {
+    
+  this.myData = null;
+  this.generate = function(object)
+     {   
+  this.typeQuotes  = $(object).data("cat")+".json";
+    console.log(this.typeQuotes);
+    var data1;
+    $.ajax({
+        url: this.typeQuotes,
+        async: false,
+        dataType: 'json',
+        success: function(data){
+            data1= data;
+        }
+    });
+       this.myData = data1;
+     };
+  this.inject = function(number){
     $(".injectQuote").empty();
-     $.getJSON(typeQuotes,function(data)
-     {
-       data.forEach(function(element)
+         var abc = this.myData.length;
+           $("#myLength").attr("data-myLength",abc);
+               
+       for(i = number-20;i<number;i++)
        {
-         var item = '<div class="box col-md-3" ><h4>'+element.text+'</h4><p>'+element.author+'</p></div>';
+        
+         var item = '<div class="box col-md-3" ><h4 >'+this.myData[i].text+'</h4><p>'+this.myData[i].author+'</p></div>';
          $(".injectQuote").append(item);
+       }
+    };
+  }
 
-       });
-      
 
-       
-     });
+function GeneratePagination()
+{
+  console.log($("#myLength").data("mylength"));
+  var totalQuotes = $("#myLength").data("mylength");
+
+  
+  var page = "<ul class='page-container'>";
+  for(var i = 1; i<((totalQuotes)/15)-2;i++)
+  {
+    page += "<li class='page-item'>"+i+"</li>";
+  }
+  page+="</ul>";
+ // console.log(page);
+  $("#pagination").append(page);
 }
--- src/quotes/quotes.js
+++ src/quotes/quotes.js
@@ -1,12 +1,24 @@
 $(document).ready(function()
 {
+ 
   var quotes= $.getJSON("quoteList.json", function(data)
-    {
-          console.log(data.length);
-       appendQuote(data);
-
-        $(".box").on("click",function(){
-                showQuotes(this);
-        });
+  {
+   console.log(data.length);
+   appendQuote(data);
+   var myQuotes = new showQuotes();
+    $(".categories").on("click",function(){      
+          myQuotes.generate(this);
+          myQuotes.inject(20);
+         GeneratePagination();
+    
+   $('.page-item').on('click',function()
+         {
+           console.log("page-item clicked");
+      
+           var number =Number( $(this).html());
+           myQuotes.inject(number*20);
+      });
     });
- });
+  });
+});
+
